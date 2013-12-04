00a046adf8796a411f275bfd86cc609b59c99459
Dariusz Krol <dkrol@agh.edu.pl>
2013-12-04 17:12:37 +0100

    Moving to Zurb foundation 5.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,7 @@
 //= require jit-yc
 
 
-$(function(){ $(document).foundation(); });
+$(document).foundation();
 
 function string_with_delimeters() {
     var string_copy = this.split("").reverse().join("");
