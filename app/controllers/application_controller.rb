6b1cb6af0dc47167d187670d210764ba4d51542f
Jakub Liput <jakub.liput@gmail.com>
2013-12-07 14:05:17 +0100

    Login with Google OpenID - restricted to user email: ScalarmUser.openid_google_email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
 
-  before_filter :authenticate, :except => [:login]
+  before_filter :authenticate, :except => [:login, :login_openid_google, :openid_callback_google]
 
   protected
 
--- app/controllers/user_controller_controller.rb
+++ app/controllers/user_controller_controller.rb
@@ -1,5 +1,15 @@
+require 'openid'
+require 'openid/extensions/ax'
+
 class UserControllerController < ApplicationController
+  include UserControllerHelper
+
+  def successful_login_path
+    experiments_path
+  end
+
   def login
+    Rails.logger.debug("Flash #{flash[:error]}")
     if request.post?
       begin
         session[:user] = ScalarmUser.authenticate_with_password(params[:username], params[:password]).id
@@ -7,7 +17,7 @@ class UserControllerController < ApplicationController
 
         flash[:notice] = t('login_success')
 
-        redirect_to experiments_path
+        redirect_to successful_login_path
       rescue Exception => e
         Rails.logger.debug("Exception: #{e}")
         reset_session
@@ -41,4 +51,131 @@ class UserControllerController < ApplicationController
 
     redirect_to :action => 'account'
   end
+
+  # --- OpenID support ---
+
+  @@openid_ax_email_url = 'http://schema.openid.net/contact/email'
+  @@openid_ax_email_alias = 'email'
+
+  # Invoke Google OpenID login procedure.
+  def login_openid_google
+    google_oid_url = 'https://www.google.com/accounts/o8/id'
+
+    begin
+      oidreq = consumer.begin(google_oid_url)
+    rescue OpenID::OpenIDError => e
+      flash[:error] = t('openid.provider_discovery_failed', provider_url: google_oid_url,
+                         error: e.to_s)
+      redirect_to login_path
+      return
+    end
+
+    # -- Attribute Exchange support --
+    axreq =  OpenID::AX::FetchRequest.new
+    attr_email = OpenID::AX::AttrInfo.new(@@openid_ax_email_url, @@openid_ax_email_alias, true)
+    attr_email.required = true
+    axreq.add(attr_email)
+    oidreq.add_extension(axreq)
+
+    return_to = openid_callback_google_url
+    realm = login_url
+
+    if oidreq.send_redirect?(realm, return_to)
+      redirect_to oidreq.redirect_url(realm, return_to)
+    else
+      render :text => oidreq.html_markup(realm, return_to, false, {'id' => 'openid_form'})
+    end
+  end
+
+  # Action for callback from Google OpenID.
+  def openid_callback_google
+    restrict_email = 'jakub.liput@gmail.com'
+
+    Rails.logger.debug("Google OpenID callback with parameters: #{params}")
+
+    parameters = params.reject{|k,v|request.path_parameters[k]}
+    parameters.reject!{|k,v|%w{action controller}.include? k.to_s}
+    oidresp = consumer.complete(parameters, openid_callback_google_url)
+
+    # Got OID response, check status
+    case oidresp.status
+
+      when OpenID::Consumer::SUCCESS
+
+        # -- Attribute Exchange support --
+        ax_resp = OpenID::AX::FetchResponse.from_success_response(oidresp)
+
+        # manually add alias to response
+        ax_resp.aliases.add_alias(@@openid_ax_email_url, @@openid_ax_email_alias)
+
+        resp_email = ax_resp.get_extension_args['value.email']
+
+        if resp_email
+          resp_identity = params['openid.identity']
+          Rails.logger.debug("User logged in with OpenID identity: #{resp_identity}")
+
+          # FIXME: Remove temporary restriction for one user from openid
+          if resp_email == restrict_email
+
+            # TODO: method name?
+            user = ScalarmUser.find_by('openid_google_email', resp_email)
+
+            if user
+              flash[:notice] = t('openid.verification_success', identity: oidresp.display_identifier)
+              session[:user] = user._id
+              redirect_to successful_login_path
+            else
+              flash[:error] = t('openid.google.no_openid_user', email: resp_email)
+              redirect_to login_path
+            end
+
+          else # only for test-users
+
+            flash[:error] = t('openid.google.tmp_restrict')
+            redirect_to login_path
+
+          end
+
+        else
+          flash[:error] = t('openid.google.no_email_provided')
+          Rails.logger.error("Error authenticating with OpenID: no email provided by OpenID Server. Response: #{params}")
+          redirect_to login_path
+        end
+
+      when OpenID::Consumer::FAILURE
+        if oidresp.display_identifier
+          flash[:error] = t('openid.verification_failed_identity', identity: oidresp.display_identifier,
+                            message: oidresp.message)
+        else
+          flash[:error] = t('openid.verification_failed', message: oidresp.message)
+        end
+        redirect_to login_path
+
+      when OpenID::Consumer::CANCEL
+        flash[:error] = t('openid.cancelled')
+        redirect_to login_path
+
+      ## Please uncomment below state handler when using OpenID immediate mode
+      ## and OpenID direct user url
+      #when OpenID::Consumer::SETUP_NEEDED
+      #  msg = "OpenID immediate request failed - Setup Needed"
+      #  flash[:alert] = msg
+
+      else
+        flash[:error] = t('openid.unknown_status', status: oidresp.status)
+        redirect_to login_path
+
+    end
+  end
+
+  private
+
+  # Get stateless mode OpenID::Consumer instance for this controller.
+  def consumer
+    if @consumer.nil?
+      @consumer = OpenID::Consumer.new(session, nil) # 'nil' for stateless mode
+    end
+    return @consumer
+  end
+
 end
