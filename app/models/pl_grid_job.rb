bb6367f7457ea6ed04a35e3c2caf9a6473121756
Dariusz Krol <dkrol@agh.edu.pl>
2014-03-26 11:45:52 +0100

    Bugfixing.

--- app/models/pl_grid_job.rb
+++ app/models/pl_grid_job.rb
@@ -48,9 +48,9 @@ class PlGridJob < MongoActiveRecord
 
     if walltime < 60
       55
-    elsif walltime > 60 and walltime < 72*60
+    elsif walltime >= 60 and walltime < 72*60
       72*60 - 5
-    elsif walltime > 72*60
+    elsif walltime >= 72*60
       168*60 - 5
     end
   end
