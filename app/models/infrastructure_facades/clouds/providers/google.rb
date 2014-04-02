f68ab8b092310a4a13e17400fd70a47ee3d8da21
Jakub Liput <jakub.liput@gmail.com>
2014-04-02 15:06:53 +0200

    Working Google Compute Cloud client

--- app/models/infrastructure_facades/clouds/providers/google.rb
+++ app/models/infrastructure_facades/clouds/providers/google.rb
@@ -14,41 +14,6 @@ module GoogleCloud
     DEFAULT_ZONE = 'us-central1-a'
     BASE_URL = "https://www.googleapis.com/compute/#{API_VERSION}/projects"
 
-#    def network_url(project, network_name)
-#      "#{BASE_URL}/#{project}/global/networks/default"
-#    end
-#
-#    API_VERSION = 'v1'
-#    BASE_URL = "https://www.googleapis.com/compute/#{API_VERSION}/projects"
-#    DEFAULT_ZONE_NAME = 'us-central1-a'
-#
-#    # DEFAULT_ZONE_URL = BASE_URL + DEFAULT_PROJECT + '/global/zones/' + DEFAULT_ZONE_NAME
-#
-#    #DEFAULT_MACHINE = "#{BASE_URL}/#{DEFAULT_PROJECT}/zones/#{DEFAULT_ZONE_NAME}/machineTypes/#{DEFAULT_INSTANCE_TYPE}"
-#    #DEFAULT_NETWORK = "#{BASE_URL}/#{DEFAULT_PROJECT}/global/networks/default"
-#
-#    secret_google_email = '724529945327@developer.gserviceaccount.com'
-#
-## Creating a new API client and loading the Google Compute Engine API.
-#
-#    path_to_key_file ="privatekey.p12"
-#    secret_passphrase = "notasecret"
-#    secret_key_content = nil
-#    File::open(path_to_key_file, 'rb') {|f| secret_key_content = f.read}
-#    raise 'no key content' if secret_key_content.nil?
-#    key = Google::APIClient::KeyUtils.load_from_pkcs12(secret_key_content, secret_passphrase)
-#
-#    asserter = Google::APIClient::JWTAsserter.new(
-#        secret_google_email,
-#        'https://www.googleapis.com/auth/compute',
-#        key)
-#
-#    client.authorization = asserter.authorize()
-#
-#    compute = client.discovered_api('compute', API_VERSION)
-
-
-
     def initialize(secrets)
       super(secrets)
       @api_client = Google::APIClient.new(application_name: 'scalarm', application_version: 1)
@@ -67,98 +32,112 @@ module GoogleCloud
       'Google Compute Engine'
     end
 
+    # TODO decision: ids -> resources urls?
+
     # TODO error handling method?
 
     # TODO images can be fetch from various sources (eg. debian-cloud) and user repo
     # TODO which we should use?
     def all_images_info
       result = execute @compute_api.images.list, project: @secrets.project
-      raise result.error_message if result.error?
-      result_hash = JSON.parse(result.body)
-      Hash[result_hash['items'].map {|i| [i['selfLink'], i['name']]}]
+      result = parse_result_or_raise_error(result)
+      Hash[result['items'].map {|i| [i['selfLink'], i['name']]}]
     end
 
     def all_vm_ids
       result = execute @compute_api.instances.list, project: @secrets.project, zone: DEFAULT_ZONE
-      raise result.error_message if result.error?
-      result_hash = JSON.parse(result.body)
-      result_hash['items'].map {|i| i['selfLink']}
+      result = parse_result_or_raise_error(result)
+      result.has_key?('items') ? result['items'].map {|i| i['name']} : []
     end
 
+    # FIXME no network param?
+    # Blocks until all insert requests are done. Sends requests for each VM in separate thread.
+    # @param [Hash] params additional params hash (Symbol => String): instance_type: one of self.instance_types string
     def instantiate_vms(base_name, image_id, number, params)
-      result = execute @compute_api.instances.insert, {project: @secrets.project, zone: DEFAULT_ZONE}, {
-          'machineType' => params[:instance_type],
-          name: "scalarm-#{SecureRandom.uuid}",
-          'networkingInterfaces' => [ {'accessConfigs'=>[{name: 'External NAT', type: 'ONE_TO_ONE_NAT'}],
-                                      network: network_url} ],
-          disks: [ {boot: true, type: 'PERSISTENT', mode: 'READ_WRITE', 'autoDelete'=>true, 'initializeParams'=>{
-              'sourceImage'=>image_url(image_id)
-          }} ]
-      }
-      #done_result = wait_for_done(result_hash['name'])
-      # disk 'deviceName'
+      throw 'no instance type specified' unless params.include?(:instance_type)
+      ids_array_lock = Mutex.new
+      ids_array = []
+      threads = (1..number).map do
+        Thread.start do
+          instance_name = generate_instance_name(base_name)
+
+          body = instances_insert_body(instance_name, machine_type_url(params[:instance_type]), network_url, image_id)
+          puts "body for #{base_name} #{image_id}: #{body}"
+
+          insert_result = execute(@compute_api.instances.insert, {project: @secrets.project, zone: DEFAULT_ZONE}, body)
+          insert_result = parse_result_or_raise_error(insert_result)
+          done_result = wait_for_done(insert_result['name'])
+
+          instance_info = get_instance_info(instance_name)
+          ids_array_lock.synchronize do
+            ids_array = parse_result_or_raise_error(instance_info)['name'] # TODO !
+          end
+        end
+      end
+      threads.map &:join
+
+      ids_array
     end
 
 
     ## -- VM instance methods --
+    # initializing, running, deactivated, error
+
+    #PROVISIONING - Resources are being reserved for the instance. The instance isn't running yet.
+    #STAGING - Resources have been acquired and the instance is being prepared for launch.
+    #RUNNING - The instance is booting up or running. You should be able to ssh into the instance soon, though not immediately, after it enters this state.
+    #STOPPING - The instance is being stopped either due to a failure, or the instance being shut down. This is a temporary status and the instance will move to either PROVISIONING or TERMINATED.
+    #STOPPING - The instance is in the process of being stopped.
+    #TERMINATED - The instance either failed for some reason or was shutdown. This is a permanent status, and the only way to repair the instance is to delete and recreate it.
 
     STATES_MAPPING = {
-      pending: :initializing,
-      running: :running,
-      shutting_down: :deactivated,
-      terminated: :deactivated,
-      stopping: :deactivated,
-      stopped: :deactivated
+      "PROVISIONING"=> :initializing,
+      "STAGING"=> :initializing,
+      "RUNNING"=> :running,
+      "STOPPING"=> :deactivated,
+      "STOPPED"=> :deactivated,
+      "TERMINATED"=> :deactivated
     }
 
     def status(id)
-      STATES_MAPPING[ec2_instance(id).status]
+      STATES_MAPPING[parse_result_or_raise_error(get_instance_info(id))['status']]
     end
 
     def terminate(id)
-      ec2_instance(id).terminate
+      parse_result_or_raise_error(instance_delete(id))
     end
 
+    # TODO WARNING: instance remains in RUNNING mode through the reset
     def reinitialize(id)
-      ec2_instance(id).reboot
+      parse_result_or_raise_error(instance_reset(id))
     end
 
     # @return [Hash] {:ip => string cloud public ip, :port => string redirected port} or nil on error
     def public_ssh_address(id)
-      {ip: ec2_instance(id).public_dns_name, port: '22'}
-    end
-
-    # TODO: translate or remove
-    def vm_record_info(vm_record)
-      "Type: #{instance_type}"
-    end
-
-    def exists?(id)
-      ec2_instance(id).exists?
+      {
+          ip: parse_result_or_raise_error(get_instance_info(id))['networkInterfaces'][0]['accessConfigs'][0]['natIP'],
+          port: '22'
+      }
     end
 
     # TODO
     def self.instance_types
       {
-          'f1.micro'=> 'Micro (TODO)'
+          'f1-micro'=> 'Micro (TODO)'
       }
     end
 
     # --- Utils ---
 
-    def instance_name
-      ''
+
+    # TODO
+    def generate_instance_name(base)
+      "scalarm-#{SecureRandom.uuid}"
     end
 
     def operation_status(operation_name)
-      result = client.execute(
-          api_method: @compute_api.zone_operations.get,
-          parameters: {
-              project: @secrets.project,
-              zone: DEFAULT_ZONE_NAME,
-              operation: operation_name
-          }
-      )
+      result = execute @compute_api.zone_operations.get, project: @secrets.project, zone: DEFAULT_ZONE,
+                       operation: operation_name
       JSON.parse(result.body)['status']
     end
 
@@ -172,7 +151,7 @@ module GoogleCloud
     def execute(method, parameters, body=nil)
       query = {api_method: method}
       query.merge!({parameters: parameters})
-      query.merge!({body: body}) if body
+      query.merge!({body_object: body}) if body
 
       result = @api_client.execute(query)
     end
@@ -182,7 +161,51 @@ module GoogleCloud
     end
 
     def image_url(image_id)
-      "#{BASE}/#{@secrets.project}/global/images/#{image_id}"
+      "#{BASE_URL}/#{@secrets.project}/global/images/#{image_id}"
+    end
+
+    def machine_type_url(machine_type)
+      "#{BASE_URL}/#{@secrets.project}/zones/#{DEFAULT_ZONE}/machineTypes/#{machine_type}"
+    end
+
+    def parse_result_or_raise_error(result)
+      raise result.error_message if result.error?
+      JSON.parse(result.body)
+    end
+
+    def get_instance_info(instance_name)
+      execute @compute_api.instances.get, instance: instance_name, project: @secrets.project, zone: DEFAULT_ZONE
+    end
+
+    def instance_delete(instance_name)
+      execute @compute_api.instances.delete, instance: instance_name, project: @secrets.project, zone: DEFAULT_ZONE
+    end
+
+    # Google doc: Performing a reset on your instance is similar to pressing the reset button on your computer.
+    # Note that your instance remains in RUNNING mode through the reset.
+    def instance_reset(instance_name)
+      execute @compute_api.instances.reset, instance: instance_name, project: @secrets.project, zone: DEFAULT_ZONE
+    end
+
+    def instances_insert_body(instance_name, machine_type_url, network_url, image_id)
+      {
+          machineType: machine_type_url,
+          name: instance_name,
+          networkInterfaces: [ {accessConfigs: [{name: 'External NAT', type: 'ONE_TO_ONE_NAT'}],
+                                   network: network_url} ],
+          disks: [ {boot: true, type: 'PERSISTENT', mode: 'READ_WRITE', autoDelete: true,
+                    deviceName: 'scalarm-root',
+                    initializeParams: {sourceImage: image_id}
+                   } ]
+      }
+
+      #{
+      # 'machineType' => DEFAULT_MACHINE,
+      # 'name'  => DEFAULT_INSTANCE_NAME,
+      # "networkInterfaces" => [ { "accessConfigs"=> [ { "name"=> "External NAT", "type"=> "ONE_TO_ONE_NAT" } ], "network"=> "https://www.googleapis.com/compute/v1/projects/scalarm-tests-1/global/networks/default" } ],
+      # "disks"=> [ { "boot"=> true, "type"=> "PERSISTENT", "mode"=> "READ_WRITE", "deviceName"=> "rest-test",  'autoDelete'=>true, "initializeParams" => {"sourceImage"=> 'https://www.googleapis.com/compute/v1/projects/scalarm-tests-1/global/images/scalarm-1'} }
+      #           ],
+      #}
     end
 
   end
