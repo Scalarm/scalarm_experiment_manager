39f729e3168a31a6b263c2e5f26b0df0a4b0c83c
Jakub Liput <jakub.liput@gmail.com>
2014-06-02 11:57:16 +0200

    Logging infrastructure logger messages to log/infrastructures.log

--- app/models/infrastructure_facades/infrastructure_task_logger.rb
+++ app/models/infrastructure_facades/infrastructure_task_logger.rb
@@ -1,4 +1,20 @@
+class InfrastructureTaskFormatter
+  def call(severity, time, progname, msg)
+    "#{msg.strip}\n"
+  end
+end
+
 class InfrastructureTaskLogger
+  @@mutex = Mutex.new
+  @@infrastructures_logger = Logger.new("#{Rails.root}/log/infrastructures.log")
+  @@infrastructures_logger.formatter = InfrastructureTaskFormatter.new
+
+  def self.logger
+    @@mutex.synchronize do
+      @@infrastructures_logger
+    end
+  end
+
   def initialize(infrastructure_name, task_id=nil)
     if task_id
       @log_format = Proc.new do |message|
@@ -13,7 +29,7 @@ class InfrastructureTaskLogger
 
   def method_missing(method_name, *args, &block)
     if %w(info debug warn error).include? method_name.to_s
-      Rails.logger.send(method_name.to_s, @log_format.call(args[0], block))
+      InfrastructureTaskLogger.logger.send(method_name.to_s, @log_format.call(args[0], block))
     else
       super(method_name, *args, &block)
     end
