e2803238ffccc03e5d0b4253b56b28fbaa8b4b64
Jakub Liput <jakub.liput@gmail.com>
2014-08-14 18:45:04 +0200

    SCAL-306: checked and reorganized controller tests

--- test/controllers/experiments_controller_test.rb
+++ test/controllers/experiments_controller_test.rb
@@ -2,13 +2,9 @@ require 'test_helper'
 
 class ExperimentsControllerTest < ActionController::TestCase
 
-  test "experiment_size" do
-    #post(:calculate_experiment_size)
-  end
-
-  def test_get_all_for_anonymous
-
-  end
-
+  # TODO?
+  # test "experiment_size" do
+  #   #post(:calculate_experiment_size)
+  # end
 
 end
