44e43ffc16c0ba4a081e7cf8731fa633b14e7ba8
Dariusz Król <dkrol@agh.edu.pl>
2014-08-31 10:38:30 +0200

    SCAL-355: locking start and complete simulation run operations when extending an experiment; storing information in simulation run about simulation manager which executes it, and setting a started simulation run to be in "to send" state when stopping a simulation manager.

--- app/controllers/infrastructures_controller.rb
+++ app/controllers/infrastructures_controller.rb
@@ -215,13 +215,26 @@ class InfrastructuresController < ApplicationController
       command = params[:command]
       if %w(stop restart destroy_record).include? command
         yield_simulation_manager(params[:record_id], params[:infrastructure_name]) do |sm|
-          sm.send(params[:command])
-          # destroy temp password
+          # destroy temp password and stop a started simulation run if any
           if %w(stop destroy_record).include? command
             unless (temp_pass = SimulationManagerTempPassword.find_by_sm_uuid(sm.record.sm_uuid)).blank?
+
+              unless temp_pass.experiment_id.nil? or sm.record.sm_uuid.nil?
+                started_simulation_run = Experiment.find_by_id(temp_pass.experiment_id).simulation_runs.
+                    where(sm_uuid: sm.record.sm_uuid, to_sent: false, is_done: false).first
+
+                unless started_simulation_run.nil?
+                  started_simulation_run.to_sent = true
+                  started_simulation_run.save
+                end
+
+              end
+
               temp_pass.destroy
             end
           end
+          sm.send(params[:command])
+
         end
         render json: {status: 'ok', msg: I18n.t('infrastructures_controller.command_executed', command: params[:command])}
       else
