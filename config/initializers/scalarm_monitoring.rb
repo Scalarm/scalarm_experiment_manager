2cda4aca72d48dc168a36dde15c0b784e8c3d936
Jakub Liput <jakub.liput@gmail.com>
2014-10-09 14:44:49 +0200

    Reverting accidential development change

--- config/initializers/scalarm_monitoring.rb
+++ config/initializers/scalarm_monitoring.rb
@@ -3,7 +3,4 @@ unless Rails.env.test?
 
   # SiM monitoring threads are now started in Rakefile
 	#InfrastructureFacadeFactory.start_all_monitoring_threads
-
-  InfrastructureFacadeFactory.start_monitoring_thread_for('qsub')
-
 end
