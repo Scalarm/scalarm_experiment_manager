2b9da762edf66f2bc0cd0b52294a0e034927e9b7
mariusz89016 <mariusz89016@gmail.com>
2014-12-22 15:44:24 +0100

    [SCAL-554] Moved ChartService to another repo

--- chart_service/interaction/interaction_chart_interaction.js
+++
@@ -1,6 +0,0 @@
-//variable i is defined in chart_service.js - in anonymous function (to provide local scope for 'i')
-function interaction_interaction(i) {
-	$("#interaction_chart_" + i + " circle").on("mouseenter", function() {
-		console.log("mouseenter " + i);
-	});
-}
--- chart_service/serialized_object_to_json.rb
+++
@@ -1,5 +0,0 @@
-require "json"
-require "base64"
-require "cgi"
-
-puts JSON.generate(Marshal.load(Base64.decode64(CGI::unescape(ARGV[0]))))
