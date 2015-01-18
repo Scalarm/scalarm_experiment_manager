326b2fa8a1ac23a74135df317776efcd34443cb4
mpaciore <mateusz.j.paciorek@gmail.com>
2015-01-18 22:33:41 +0100

    SCAL-475 Removed dot from date format in monitoring log file name

--- app/models/infrastructure_facades/pl_grid_facade.rb
+++ app/models/infrastructure_facades/pl_grid_facade.rb
@@ -106,7 +106,7 @@ class PlGridFacade < InfrastructureFacade
           "unxz -f #{bin_base_name}.xz",
           "chmod a+x #{bin_base_name}",
           "X509_USER_PROXY=#{remote_proxy_path} #{ShellCommands.
-              run_in_background("./#{bin_base_name}", "#{bin_base_name}_`date +%Y-%m-%d_%H-%M-%S.$(expr $(date +%N) / 1000000)
+              run_in_background("./#{bin_base_name}", "#{bin_base_name}_`date +%Y-%m-%d_%H-%M-%S-$(expr $(date +%N) / 1000000)
 `.log")}"
       )
       Rails.logger.debug("Executing scalarm_monitoring: #{ssh.exec!(cmd)}")
