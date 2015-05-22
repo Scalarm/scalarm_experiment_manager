c4221ef52d70aff74d0b09098834712a66add57d
Jakub Liput <jakub.liput@gmail.com>
2015-05-22 17:31:24 +0200

    SCAL-657: moving UserSession to scalarm-service_core

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,38 +1,4 @@
-require 'securerandom'
+require 'scalarm/service_core/user_session'
 
-require 'scalarm/database/model/user_session'
-
-# TODO: move logic to separate gem
-class UserSession < Scalarm::Database::Model::UserSession
-
-  def valid?
-    if Time.now.to_i - self.last_update.to_i > Rails.configuration.session_threshold
-      false
-    else
-      true
-    end
-  end
-
-  def self.create_and_update_session(user_id, uuid)
-    session_id = BSON::ObjectId(user_id.to_s)
-    if uuid.nil?
-      uuid = session[:session_uuid] = SecureRandom.uuid
-    end
-
-    session = (UserSession.where(session_id: session_id, uuid: uuid).first or
-      UserSession.new(session_id: session_id, uuid: uuid))
-    session.last_update = Time.now
-    session.save
-
-    session
-  end
-
-  def generate_token
-    token = SecureRandom.uuid
-    self.tokens = [] unless self.tokens
-    self.tokens << token
-    self.save
-    token
-  end
-
+class UserSession < Scalarm::ServiceCore::UserSession
 end
