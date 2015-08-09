27244f8e0aef40b5a7ae489110a229123886bc1b
mwrona <michal.wrona.4@gmail.com>
2015-08-09 19:32:00 +0200

    WSS-15 Implementation of workers scaling algorithm with basic description.

---
+++ app/models/workers_scaling/workers_scaling_algorithm.rb
@@ -0,0 +1,24 @@
+##
+# Class describing interface of Workers scaling algorithm. To add new algorithm
+# you need provide class with proper methods implemented, which also should
+# inherit this class.
+class WorkersScalingAlgorithm
+
+  NOT_IMPLEMENTED = 'This is an abstract method, which must be implemented by all subclasses'
+
+  ##
+  # Method called when algorithm is started, before first execution of
+  # #experiment_status_check. Should contains actions performed in the
+  # beginning of algorithm e.g. sending first workers on infrastructure.
+  def initial_deployment
+    raise NOT_IMPLEMENTED
+  end
+
+  ##
+  # Main algorithm loop, executed on specified event (e.g. finished simulation) or
+  # when given time since last execution passed. Should contains main algorithm logic.
+  def experiment_status_check
+    raise NOT_IMPLEMENTED
+  end
+
+end
