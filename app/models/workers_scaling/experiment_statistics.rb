4f29fcabb66ec18269dc44de12a79034f1afc6ba
Michał Wrona <michal.wrona.4@gmail.com>
2015-08-10 12:23:19 +0200

    SCAL-1065: WSS-17 Implementation of experiment statistics class
    https://jira.plgrid.pl/jira/browse/SCAL-1065
    
    Class allows to calculate various experiment statistics:
        * worker_throughput
        * system_throughput
        * makespan
    
    Previous statistics class was discarded due to its redundancy.

---
+++ app/models/workers_scaling/experiment_statistics.rb
@@ -0,0 +1,50 @@
+##
+# ExperimentStatistics class calculates various statistics about
+# experiment run and execution.
+module WorkersScaling
+  class ExperimentStatistics
+
+    ##
+    # Number of running simulations in sequential worker
+    RUNNING_SIMULATIONS = 1
+
+    def initialize(experiment, resources_interface)
+      @experiment = experiment
+      @resources_interface = resources_interface
+    end
+
+    ##
+    # Returns throughput for given worker sm_uuid. Throughout for worker is calculated as:
+    #   throughput[sim/s] = (finished_simulations + running_simulation)/(Time.now - start_time)
+    def worker_throughput(worker_sm_uuid)
+      calculate_worker_throughput @resources_interface.get_workers_records(cond: {sm_uuid: worker_sm_uuid}).first
+    end
+
+    ##
+    # Returns throughput of experiment(system) associated with ExperimentStatistics instance.
+    # System throughput is calculated as sum of all workers throughput [sim/s].
+    def system_throughput
+      @resources_interface.get_workers_records
+        .map {|worker| calculate_worker_throughput worker}
+        .reduce 0.0, :+
+    end
+
+    ##
+    # Returns makespan of experiment associated with ExperimentStatistics instance.
+    # Makespan is calculated as:
+    #   makespan[s] = simulations_to_run/system_throughput
+    def makespan
+      @experiment.reload
+      (@experiment.experiment_size - @experiment.count_done_simulations)/Float(system_throughput)
+    end
+
+    private
+
+    ##
+    # Returns throughput for given worker. For details look at #worker_throughput
+    def calculate_worker_throughput(worker)
+      ((worker.finished_simulations || 0) + RUNNING_SIMULATIONS)/Float(Time.now - worker.created_at)
+    end
+
+  end
+end
