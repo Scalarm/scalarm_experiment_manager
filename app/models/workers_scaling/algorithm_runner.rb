1cf1040c9d01efdc1b2c87e8274a48598c2aaac5
mpaciore <mateusz.j.paciorek@gmail.com>
2015-09-23 14:24:19 +0200

    SCAL-1066: WSS-18 Adding logs to Algorithm Runner, converting ids
    https://jira.plgrid.pl/jira/browse/SCAL-1066
    
    Adding logs on debug level for most important steps.
    Converting experiment_id to string before using it in cache to avoid type mismatch - BSON::ObjectId and string versions should be treated in the same way.

--- app/models/workers_scaling/algorithm_runner.rb
+++ app/models/workers_scaling/algorithm_runner.rb
@@ -8,19 +8,22 @@ module WorkersScaling
     # Returns instance of AlgorithmRunner for experiment_id if registered
     # Otherwise returns nil
     def self.get(experiment_id)
-      return @@cache[experiment_id] if @@cache
+      LOGGER.debug "Getting runner from cache, experiment id: #{experiment_id}"
+      return @@cache[experiment_id.to_s] if @@cache
     end
 
     ##
     # Registers instance of AlgorithmRunner for experiment_id
     def self.put(experiment_id, runner)
-      @@cache[experiment_id] = runner
+      @@cache[experiment_id.to_s] = runner
+      LOGGER.debug "Adding runner to cache, experiment id: #{experiment_id}"
     end
 
     ##
     # Unregisters instance of AlgorithmRunner for experiment_id
     def self.delete(experiment_id)
-      @@cache.delete(experiment_id) if @@cache
+      @@cache.delete(experiment_id.to_s) if @@cache
+      LOGGER.debug "Deleting runner from cache, experiment id: #{experiment_id}"
     end
 
     ##
@@ -42,24 +45,29 @@ module WorkersScaling
     # Stops when there is no next execution time set
     def start
       Thread.new do
-        @algorithm.initial_deployment
-        @next_execution_time = Time.now + @interval
+        begin
+          @algorithm.initial_deployment
+          @next_execution_time = Time.now + @interval
 
-        @mutex.synchronize do
-          catch :finished do
-            until @next_execution_time.nil?
+          @mutex.synchronize do
+            catch :finished do
+              until @next_execution_time.nil?
 
-              while @next_execution_time > Time.now do
-                @mutex.sleep @next_execution_time - Time.now
-                throw :finished if @next_execution_time.nil?
+                while @next_execution_time > Time.now do
+                  @mutex.sleep @next_execution_time - Time.now
+                  throw :finished if @next_execution_time.nil?
+                end
+
+                execute_and_schedule
               end
-
-              execute_and_schedule
             end
           end
-        end
 
-        self.class.delete(@experiment.id)
+          self.class.delete(@experiment.id)
+        rescue => e
+          LOGGER.error "Exception occurred during workers scaling algorithm: #{e.to_s}\n#{e.backtrace.join("\n")}"
+          raise
+        end
       end
     end
 
@@ -73,6 +81,7 @@ module WorkersScaling
         should_unlock = true
       end
 
+      LOGGER.debug "Starting experiment_status_check method, #{should_unlock ? 'asynchronous' : 'synchronous'} call"
       @algorithm.experiment_status_check
 
       @next_execution_time = if @experiment.reload.completed?
@@ -80,6 +89,7 @@ module WorkersScaling
                              else
                                Time.now + @interval
                              end
+      LOGGER.debug "Setting @next_execution_time to #{@next_execution_time.inspect}"
 
       @mutex.unlock if should_unlock
     end
