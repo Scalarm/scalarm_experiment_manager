bd2c1c2511051eb63b4de4f1a45fbbe1c0f0c705
mpaciore <mateusz.j.paciorek@gmail.com>
2015-10-06 11:06:01 +0200

    WSS-18: Changing LogFormatter class to proc
    https://trello.com/c/LI6cKnss/21

--- app/models/workers_scaling/logger.rb
+++ app/models/workers_scaling/logger.rb
@@ -1,11 +1,8 @@
 module WorkersScaling
-  class LogFormatter
-    def call(severity, time, progname, msg)
-      "[#{time.strftime('%Y-%m-%d %H:%M:%S')}] #{'%5.5s' % severity.upcase}: #{msg.strip}\n"
-    end
-  end
 
   LOGGER = Logger.new("#{Rails.root}/log/workers_scaling.log", 5, 1024*1024*100) unless defined? LOGGER
-  LOGGER.formatter = LogFormatter.new
+  LOGGER.formatter = proc do |severity, time, progname, msg|
+    "[#{time.strftime('%Y-%m-%d %H:%M:%S')}] #{'%5.5s' % severity.upcase}: #{msg.strip}\n"
+  end
 
 end
