efaa7739beec8a54610e3bcbe7df75f0361bdaf5
Jakub Liput <jakub.liput@gmail.com>
2015-11-06 16:13:05 +0100

    SCAL-1042: Footer images helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def button_classes
     'button radius small expand action-button'
   end
+
+  def footer_link_image(image_path, href='#')
+    content_tag(:div,
+                style: 'height: 40px; float: right; margin-left: 24px;',
+                class: 'footer-link-image') do
+      link_to(image_tag(image_path, class: 'in-middle fit-to-div'), href)
+    end
+  end
 end
