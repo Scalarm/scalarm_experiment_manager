6b516d32726cbde4a457043e0b9a8e1be0c2fc4f
Jakub Liput <jakub.liput@gmail.com>
2015-11-12 12:07:50 +0100

    SCAL-1138: Added /experiments/:id/schedule_multiple_points method
    This method allows extending of custom points experiment with multiple points with CSV provided as a file or string (csv parameter).
    The CSV format is the same as in starting new experiment with CSV.

--- test/integration/experiments_controller_schedule_points_test.rb
+++ test/integration/experiments_controller_schedule_points_test.rb
@@ -71,4 +71,42 @@ class ExperimentsControllerSchedulePointsTest < ActionDispatch::IntegrationTest
     assert_nil wait_response['input_parameters']
   end
 
+  # G:
+  #   There is a supervised experiment without any points
+  # W:
+  #   A multiple points are added with /experiments/:id/schedule_multiple_points {"csv": ...}
+  # T:
+  #   A multiple simulation runs for these point should be generated using /experiments/:id/next_simulation
+  test 'next_instance should return multiple scheduled points after schedule_multiple_points' do
+    # Given
+    get "/experiments/#{@experiment.id.to_s}/next_simulation"
+    assert_response :success
+
+    # When
+    scheduled_xs = (1..10).to_a
+
+    sched_csv = "x\n" + scheduled_xs.join("\n")
+
+    post "experiments/#{@experiment.id.to_s}/schedule_multiple_points.json", {csv: sched_csv}
+    assert_response :success
+
+
+    # Then
+    scheduled_xs.each do |x|
+      get "/experiments/#{@experiment.id.to_s}/next_simulation"
+      assert_response :success
+      success_response = JSON.parse(response.body)
+      assert_equal 'ok', success_response['status'], "Non-OK next_simulation response for x=#{x} point"
+      assert success_response.has_key?('input_parameters')
+      assert_equal x, success_response['input_parameters']['x'].to_i
+    end
+
+    # After all next_simulations
+    get "/experiments/#{@experiment.id.to_s}/next_simulation"
+    assert_response :success
+    wait_response = JSON.parse(response.body)
+    assert_equal 'wait', wait_response['status']
+    assert_nil wait_response['input_parameters']
+  end
+
 end
