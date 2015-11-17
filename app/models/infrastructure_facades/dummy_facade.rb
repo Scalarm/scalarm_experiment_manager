bfba00b70bb4b14a12b0347a2e3dc86e190999db
Jakub Liput <jakub.liput@gmail.com>
2015-11-17 18:02:34 +0100

    SCAL-1155: InfrastructureFacade#query_simulation_manager_records
    * All infrastructure facades now have query_simulation_manager_records which allows to get simulation manager records from db with use of parameters as in start_simulation_managers.
    * Also changed onsite_monitoring view->controller option parsing to "on" to make code cleaner in the context of stat_simulation_managers params.
    * Added some method comments

--- app/models/infrastructure_facades/dummy_facade.rb
+++ app/models/infrastructure_facades/dummy_facade.rb
@@ -24,6 +24,14 @@ class DummyFacade < InfrastructureFacade
     end
   end
 
+  def query_simulation_manager_records(user_id, experiment_id, params)
+    DummyRecord.where(
+        user_id: user_id,
+        experiment_id: experiment_id,
+        time_limit: params['time_limit'].to_i
+    ).to_a
+  end
+
   def sm_record_class
     DummyRecord
   end
