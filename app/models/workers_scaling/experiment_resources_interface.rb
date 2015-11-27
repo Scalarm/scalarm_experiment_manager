3512fbefd44c7af84412ff829b90cd70211b5c89
mpaciore <mateusz.j.paciorek@gmail.com>
2015-11-27 20:23:27 +0100

    WSS-37: TO FIXUP

--- app/models/workers_scaling/experiment_resources_interface.rb
+++ app/models/workers_scaling/experiment_resources_interface.rb
@@ -142,7 +142,7 @@ module WorkersScaling
     ##
     # Returns overall Workers count for Experiment matching given params
     def count_all_workers(params = {})
-      get_available_infrastructures
+      get_enabled_infrastructures
           .flat_map { |infrastructure| get_workers_records_count(infrastructure, params) }
           .reduce(0) { |sum, count| sum + count }
     end
@@ -150,7 +150,7 @@ module WorkersScaling
     ##
     # Returns worker record for given sm_uuid
     def get_worker_record_by_sm_uuid(sm_uuid)
-      get_available_infrastructures.map do |infrastructure|
+      get_enabled_infrastructures.map do |infrastructure|
         get_workers_records_cursor(infrastructure, cond: {sm_uuid: sm_uuid}).first
       end .detect { |worker| not worker.nil? }
     end
