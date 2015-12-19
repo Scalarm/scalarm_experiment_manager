054983d32736c15c471b2bcba0f9d80a1f59006a
mpaciore <mateusz.j.paciorek@gmail.com>
2015-12-19 23:00:14 +0100

    SCAL-1214: Fixing tests
     https://jira.plgrid.pl/jira/browse/SCAL-1214

--- test/models/simulation_manager_record_test.rb
+++ test/models/simulation_manager_record_test.rb
@@ -154,28 +154,28 @@ class SimulationManagerRecordTest < MiniTest::Test
 
   def test_has_more_simulations_to_run_field_greater_than_zero
     sm_record = MockRecord.new({})
-    sm_record.stubs(:attributes).returns('simulations_left' => 1)
+    sm_record.stubs(:simulations_left).returns(1)
 
     assert sm_record.has_more_simulations_to_run?, 'Simulation Manager has more simulations to run when simulations_left is greater than 0'
   end
 
   def test_has_more_simulations_to_run_field_equal_to_zero
     sm_record = MockRecord.new({})
-    sm_record.stubs(:attributes).returns('simulations_left' => 0)
+    sm_record.stubs(:simulations_left).returns(0)
 
     assert (not sm_record.has_more_simulations_to_run?), 'Simulation Manager has no more simulations to run when simulations_left is 0'
   end
 
   def test_has_more_simulations_to_run_field_lesser_than_zero
     sm_record = MockRecord.new({})
-    sm_record.stubs(:attributes).returns('simulations_left' => -1)
+    sm_record.stubs(:simulations_left).returns(-1)
 
     assert (not sm_record.has_more_simulations_to_run?), 'Simulation Manager has no more simulations to run when simulations_left is lesser than 0'
   end
 
   def test_has_more_simulations_to_run_field_not_existing
     sm_record = MockRecord.new({})
-    sm_record.stubs(:attributes).returns({})
+    sm_record.stubs(:simulations_left).returns(nil)
 
     assert sm_record.has_more_simulations_to_run?, 'Simulation Manager has more simulations to run when simulations_left is not present'
   end
