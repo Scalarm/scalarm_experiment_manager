4748f07d6b5eee2b11f0f54cab641ed09a0b984d
Dariusz Król <dkrol@agh.edu.pl>
2016-01-15 12:20:09 +0000

    SCAL-1195: renaming PL-Grid PBS to Zeus @ ACC Cyfronet AGH

--- app/models/infrastructure_facades/plgrid/grid_schedulers/qsub.rb
+++ app/models/infrastructure_facades/plgrid/grid_schedulers/qsub.rb
@@ -4,7 +4,7 @@ module QsubScheduler
 
   class PlGridScheduler < PlGridSchedulerBase
     def self.long_name
-      'PL-Grid PBS'
+      'Zeus @ ACC Cyfronet AGH'
     end
 
     def self.short_name
