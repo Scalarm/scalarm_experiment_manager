d17eb210ce3f34b0423aea0140464b3ced9258b9
dkrol <dkrol3@gmail.com>
2017-01-24 23:13:26 +0100

    Fixes Scalarm/scalarm#2 : additional message regarding password change.

--- app/assets/javascripts/user_controller.js.coffee
+++ app/assets/javascripts/user_controller.js.coffee
@@ -1,3 +1,5 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$ ->
+  $("#credentials .button.radius").on "click", ->
+    $("#credentials")
+      .on 'valid.fndtn.abide',   -> $("#credentials .button.radius").click()
+      .on 'invalid.fndtn.abide', -> $(this).find('[data-invalid]').blur()
--- test/controllers/user_controller_controller_test.rb
+++ test/controllers/user_controller_controller_test.rb
@@ -23,6 +23,28 @@ class UserControllerControllerTest < ActionController::TestCase
   end
 
   def test_login_access
+    tmp_user = set_account
+
+    get :account
+    assert_response :success
+
+    tmp_user.destroy
+  end
+
+  def test_account_page_contains_information_about_password_change
+    tmp_user = set_account
+
+    get :account
+    assert_response :success
+
+    assert_select '.panel.password-change-msg', I18n.t('user_controller.password_panel.password_change_message')
+
+    tmp_user.destroy
+  end
+
+  private
+
+  def set_account
     tmp_login = "tmp_user_#{rand(100)}"
     tmp_password = "pass"
 
@@ -34,10 +56,7 @@ class UserControllerControllerTest < ActionController::TestCase
 
     assert_equal session[:user], tmp_user.id.to_s, 'Session user should be the same as tmp user id'
 
-    get :account
-    assert_response :success
-
-    tmp_user.destroy
+    tmp_user
   end
 
 end
